3dd149905553e8f66064ead20349983ad2582346
unknown <luexe@yandex.ru>
2018-02-25 12:06:16 +0300

    new form with inputs

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,16 +1,66 @@
 <template>
-  <v-container grid-list-md text-xs-center fluid pa-0>
-    <v-layout row>
+  <v-app>
+  <v-container text-xs-center fluid px-0 py-0>
+    <v-layout row wrap>
       <v-flex xs12>
         <v-card>
-          <v-card-text class="display-1">Smart Cafe</v-card-text>
+          <v-card-text class="blue white--text display-1" >SmartCafe</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex xs4 offset-xs4 mt-5>
+        <v-card dark>
+          <v-flex xs12>
+            <v-card-text class="headline mb-0">Создай аккаунт</v-card-text>
+          </v-flex>
+          <v-layout>
+            <v-flex xs-6 mx-5>
+              <v-text-field
+                name="first_name"
+                label="Имя*"
+                id="first_name">
+              </v-text-field>
+            </v-flex>
+            <v-flex xs-6 mx-5>
+              <v-text-field
+                name="second_name"
+                label="Фамилия*"
+                id="second_name">
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-flex xs-12 mx-5>
+            <v-text-field
+                name="email"
+                label="E-mail*"
+                id="email">
+            </v-text-field>
+          </v-flex>
+          <v-layout>
+            <v-flex xs-6 mx-5>
+              <v-text-field
+                name="first_name"
+                label="Пароль*"
+                type="password"
+                id="first_name">
+              </v-text-field>
+            </v-flex>
+            <v-flex xs-6 mx-5>
+              <v-text-field
+                name="second_name"
+                label="Подтвердить пароль*"
+                type="password"
+                id="second_name">
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-flex xs-6 pb-3>
+            <v-btn round class="blue white--text">Регистрация</v-btn>
+          </v-flex>
         </v-card>
       </v-flex>
     </v-layout>
-    <v-layout>
-      
-    </v-layout>
   </v-container>
+  </v-app>
 </template>
 <script>
 export default {
