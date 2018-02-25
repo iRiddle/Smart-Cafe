6a39978bb7489a88d79e928e8d52dd6c72a3c02e
unknown <luexe@yandex.ru>
2018-02-25 10:59:41 +0300

    new form

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -8,55 +8,7 @@
       </v-flex>
     </v-layout>
     <v-layout>
-      <v-flex xs6 offset-xs3 mt-5>
-        <v-card>
-          <v-card-text class="headline mb-0" pt-5>Создай аккаунт</v-card-text>
-          <div class="">Здесь будет просто умопомрачительный текст о великолепной, неповторимой
-            бесподобной и просто замечательной системе автоматизации предприятий малого бизнеса в сфере питания
-          </div>
-          <v-layout row mx-4 my-4>
-            <v-text-field
-              name="first_name"
-              label="Имя"
-              placeholder ="Введите ваше имя"
-              id="first_name">
-            </v-text-field>
-            <v-text-field
-              name="second_name"
-              label="Фамилия"
-              placeholder="Введите вашу фаилию"
-              id="second_name">
-            </v-text-field>
-          </v-layout>
-          <v-flex mx-4>
-            <v-text-field
-              label="E-mail"
-              v-model="email"
-              v-on:click="selectEmail()"
-              :rules="[rules.required, rules.email]">
-            </v-text-field>
-          </v-flex>
-          <v-layout row mx-4 my-4>
-            <v-text-field
-              name="password"
-              type="password"
-              label="Пароль"
-              placeholder ="Введите пароль"
-              id="password">
-            </v-text-field>
-            <v-text-field
-              name="confirm_password"
-              type="password"
-              label="Подтвердите пароль"
-              placeholder="Введите пароль еще раз"
-              id="confirm_password">
-            </v-text-field>
-          </v-layout>
-          <div class="text-xs-center">
-            <v-btn round color="blue" class="white--text"> Регистрация</v-btn>
-          </div>
-        </v-card>
-      </v-flex>
+      
     </v-layout>
   </v-container>
 </template>
